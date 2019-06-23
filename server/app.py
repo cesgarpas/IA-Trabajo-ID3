3e1aa3fe563c4f711b66d7e8768204efd90cf2ea
César García <cgpcsjcmp@gmail.com>
2019-06-24 01:44:49 +0200

    ADD: Web page form working

--- server/app.py
+++ server/app.py
@@ -1,6 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return render_template('landing.html')
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return "Correcto!"
+    else:
+        print("GET")
+        return render_template('form.html')
+
+
+
